6d4d74be8e2ff6723518b91d95af33991ca89554
Vincent den Boer <vincent@youapt.eu>
2021-01-24 20:12:57 +0100

    Set up Gatsby site with TypeSript config

---
+++ website/src/components/header.tsx
@@ -0,0 +1,8 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const Header = () => (
+  <header>Header</header>
+)
+
+export default Header
---
+++ website/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+  </Layout>
+)
+
+export default IndexPage
