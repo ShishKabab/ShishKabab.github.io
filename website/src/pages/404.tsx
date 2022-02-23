e9932b07a636e7900680401484884a8f0d59d1cf
Vincent den Boer <vincent@youapt.eu>
2022-02-23 11:06:18 +0100

    Make 404 page funny in a nerdy kind of way

--- website/src/pages/404.tsx
+++ website/src/pages/404.tsx
@@ -1,14 +1,20 @@
 import React from "react"
+import Heading from "../components/heading"
 
 import Layout from "../components/layout"
+import Paragraph from "../components/paragraph"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <SEO title="404: Page not found" />
+        <Heading>404: Page not found - Infinite Paradox</Heading>
+        <Paragraph>
+            You just landed on a page that doesn't exist, which means you aren't
+            really seeing any page. Or are you? Anyway, you probably just
+            followed a broken link. Sorry for that!
+        </Paragraph>
+    </Layout>
 )
 
 export default NotFoundPage
