7aa5e64bd4509c45162fd19806e1e86c4fd6b151
Vincent den Boer <vincent@youapt.eu>
2023-03-12 16:20:18 +0000

    Add co-working pics and improve description

---
+++ website/src/components/coworking-pics.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+export function CoworkingPics() {
+  return <Container>
+    <PicContainer>
+      <Pic src="/coworking-1.jpg" />
+      <div style={{ width: '20px' }}></div>
+      <Pic src="/coworking-2.jpg" />
+    </PicContainer>
+    <Caption>Space in progress, eager to evolve</Caption>
+  </Container>
+}
+
+function Pic(props: { src: string }) {
+  return <ImageLink href={props.src} target="_blank"><Image src={props.src} /></ImageLink>
+}
+
+const Container = styled.div`
+  margin-bottom: 20px;
+`
+
+const PicContainer = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 100%;
+  align-items: center;
+`
+
+const Caption = styled.div`
+  font-size: 12px;
+  text-align: center;
+`
+
+const ImageLink = styled.a`
+  width: auto
+  height: auto;
+`
+
+const Image = styled.img`
+width: 100%;
+height: auto;
+`
